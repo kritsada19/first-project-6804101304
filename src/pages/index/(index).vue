95aeca7a1b0072c2d6cdec9ec57cb01aa8fd2a1a
kritsada19 <ninep9p2006@gmail.com>
2026-07-17 14:51:16 +0700

    add thai

--- src/pages/index/(index).vue
+++ src/pages/index/(index).vue
@@ -2,15 +2,15 @@
 
   <div class="q-pa-md" style="max-width: 400px">
     <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-      <q-input filled v-model="name" label="Your name *" hint="Name and surname" lazy-rules
+      <q-input filled v-model="name" label="Your name(ชื่อ) *" hint="Name and surname" lazy-rules
         :rules="[val => (val && val.length > 0) || 'Please type something']" />
 
-      <q-input filled type="number" v-model.number="age" label="Your age *" lazy-rules :rules="[
+      <q-input filled type="number" v-model.number="age" label="Your age(อายุ) *" lazy-rules :rules="[
         val => (val !== null && val !== '') || 'Please type your age',
         val => (val > 0 && val < 100) || 'Please type a real age'
       ]" />
 
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+      <q-toggle v-model="accept" label="I accept the license and terms (ฉันยอมรับเงื่อนไข)" />
 
       <div>
         <q-btn label="Submit" type="submit" color="primary" />
